134ef7126c4da142e45dca8214ea169686d92c74
waizbart <guilhermewaizbart@gmail.com>
2024-05-15 21:22:48 -0300

    display class

--- utils.py
+++ utils.py
@@ -16,3 +16,10 @@ def is_circle_inside_circle(circle1, circle2):
     distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
     
     return distance + r1 <= r2 or distance + r2 <= r1
+
+def generate_food(ecosystem, amount, width, height):
+    for _ in range(amount):
+        x = randint(0, width)
+        y = randint(0, height)
+        
+        ecosystem.add_food((x, y))
